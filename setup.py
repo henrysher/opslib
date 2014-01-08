d74efd4928b11c2aa5840ab9b9b61d98cc846d17
Henry Huang <henry.s.huang@gmail.com>
2014-01-09 00:39:07 +0800

    Update the details into setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import opslib
 
 setup(name='opslib',
-      version='0.0.1',
-      description='Library for ICS Operations',
+      version=opslib.__version__,
+      description='Library for AWS Operations',
       packages=['opslib', 'opslib.icsutils'],
-      package_data = {"opslib": ["opslib.ini"]},
+      package_data={"opslib": ["opslib.ini"]},
+      author = "Henry Huang",
+      author_email = "henry.s.huang@gmail.com",
+      url="https://github.com/henrysher/opslib",
       license='Apache 2.0',
+      include_package_data=True,
+      classifiers= [
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Installation/Setup',
+          'Topic :: Utilities',
+      ],
       install_requires=[
         'setuptools',
         'boto',
+        'botocore',
+        'argcomplete',
       ],
      )
