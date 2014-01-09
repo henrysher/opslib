e509f215d4d70bfd34d3bfc7793eb07e21f9ffca
Henry Huang <henry.s.huang@gmail.com>
2014-01-10 00:38:28 +0800

    Refine the document

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# OpsCommon documentation build configuration file, created by
+# OpsLib documentation build configuration file, created by
 # sphinx-quickstart on Sun May  5 02:09:48 2013.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Ops Common Library'
+project = u'OpsLib Library'
 copyright = u'2013, Henry Huang'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -168,7 +168,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'OpsCommondoc'
+htmlhelp_basename = 'OpsLibdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -187,7 +187,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'OpsCommon.tex', u'OpsCommon Documentation',
+  ('index', 'OpsLib.tex', u'OpsLib Documentation',
    u'Henry Huang', 'manual'),
 ]
 
@@ -217,7 +217,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'OpsCommon', u'OpsCommon Documentation',
+    ('index', 'OpsLib', u'OpsLib Documentation',
      [u'Henry Huang'], 1)
 ]
 
@@ -231,8 +231,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'OpsCommon', u'ICS Ops Common Library Documentation',
-   u'Henry Huang', 'OpsCommon', 'Common Library for ICS Ops Team',
+  ('index', 'OpsLib', u'Library for AWS Operations Documentation',
+   u'Henry Huang', 'OpsLib', 'Common Library for AWS Operations',
    'Miscellaneous'),
 ]
 
